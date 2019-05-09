ae68b80943fac80a55ab12caaa338c777e94ba16
AbhishekSkullbear <abhishekchandra2005@gmail.com>
2019-05-09 01:00:56 -0400

    basic cell absorption

---
+++ GBC2019/Assets/Scripts/Core/CellProperties/MoveCellToBlockCenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCellToBlockCenter : MonoBehaviour
+{
+    public Block block;
+
+    Cell cell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cell = GetComponent<Cell>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cell)
+        {
+            cell.SetGridPosition(block.GetShape().aabbCenter);
+        }
+    }
+}
---
+++ GBC2019/Assets/Scripts/PlayerControls/AbsorbControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsorbControls : MonoBehaviour
+{
+    public string yellow = "Yellow1";
+    public string red = "Red1";
+    public string blue = "Blue1";
+    public string green = "Green1";
+
+    AbsorbCells absorbCells;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        absorbCells = GetComponent<AbsorbCells>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown(yellow) && absorbCells != null)
+        {
+            absorbCells.Absorb(Block.CellSubType.Y);
+        }
+        if (Input.GetButtonDown(red) && absorbCells != null)
+        {
+            absorbCells.Absorb(Block.CellSubType.R);
+        }
+        if (Input.GetButtonDown(blue) && absorbCells != null)
+        {
+            absorbCells.Absorb(Block.CellSubType.B);
+        }
+        if (Input.GetButtonDown(green) && absorbCells != null)
+        {
+            absorbCells.Absorb(Block.CellSubType.G);
+        }
+    }
+}
